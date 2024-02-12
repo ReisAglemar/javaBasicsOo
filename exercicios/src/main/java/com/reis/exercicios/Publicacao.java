2de9e6c31d6a4613e2d81b505970e17880567347
Reis <aglemar.reis@gmail.com>
2024-02-12 16:50:20 -0300

    Ajuste na indentação

--- exercicios/src/main/java/com/reis/exercicios/Publicacao.java
+++ exercicios/src/main/java/com/reis/exercicios/Publicacao.java
@@ -5,10 +5,14 @@ package com.reis.exercicios;
  * @author reis
  */
 public interface Publicacao {
-    
+
     public void abrir();
+
     public void fechar();
+
     public void folhar();
+
     public void avançarPagina();
-    public void voltarPagina();    
+
+    public void voltarPagina();
 }
