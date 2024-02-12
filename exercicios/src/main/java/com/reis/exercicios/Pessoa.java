616ee2a7ebb0a65374da8964dc12ba1987354838
Reis <aglemar.reis@gmail.com>
2024-02-11 22:02:56 -0300

    Implementado a classe Pessoa

--- exercicios/src/main/java/com/reis/exercicios/Pessoa.java
+++ exercicios/src/main/java/com/reis/exercicios/Pessoa.java
@@ -5,5 +5,46 @@ package com.reis.exercicios;
  * @author reis
  */
 public class Pessoa {
-    
+
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public Pessoa(String nome, int idade, char sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void fazerAniversario() {
+
+        System.out.println("");
+        System.out.println("    FELIZ ANIVERSÁRIO!");
+        System.out.println("");
+    }
+
 }
