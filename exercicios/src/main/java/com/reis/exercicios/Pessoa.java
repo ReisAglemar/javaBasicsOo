17cb736684af875aee54c409b8ff10765651447f
Reis <aglemar.reis@gmail.com>
2024-02-12 16:49:48 -0300

    Adicionado Id

--- exercicios/src/main/java/com/reis/exercicios/Pessoa.java
+++ exercicios/src/main/java/com/reis/exercicios/Pessoa.java
@@ -7,11 +7,12 @@ package com.reis.exercicios;
 public class Pessoa {
 
     private String nome;
-    private int idade;
+    private int idade, id;
     private char sexo;
 
-    public Pessoa(String nome, int idade, char sexo) {
+    public Pessoa(String nome, int id, int idade, char sexo) {
         this.nome = nome;
+        this.id = id;
         this.idade = idade;
         this.sexo = sexo;
     }
@@ -22,6 +23,10 @@ public class Pessoa {
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setSexo(char sexo) {
@@ -36,15 +41,33 @@ public class Pessoa {
         return idade;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public char getSexo() {
         return sexo;
     }
 
     public void fazerAniversario() {
 
+        setIdade(getIdade() + 1);
         System.out.println("");
-        System.out.println("    FELIZ ANIVERSÁRIO!");
+        System.out.println("    FELIZ ANIVERSÁRIO!!!");
+        System.out.println("    " + getNome() + ", parabéns pelos seus " + getIdade() + " anos.");
         System.out.println("");
     }
 
+    public void informacao() {
+
+        System.out.println("");
+        System.out.println("========== INFORMAÇÕES SOBRE O USUÁRIO ===========");
+        System.out.println("");
+        System.out.println("    Nome do usuário: " + getNome());
+        System.out.println("    Idade do usuário: " + getIdade());
+        System.out.println("    ID do usuário: " + getId());
+        System.out.println("    Sexo do usuário: " + getSexo());
+        System.out.println("");
+        System.out.println("------------------------------------------------");
+    }
 }
