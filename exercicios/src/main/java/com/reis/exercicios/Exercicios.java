64f5cc47ea4feab430d39abaf30be25991283435
Reis <aglemar.reis@gmail.com>
2024-02-11 22:01:43 -0300

    Adicionadas novas chamadas de métodos

--- exercicios/src/main/java/com/reis/exercicios/Exercicios.java
+++ exercicios/src/main/java/com/reis/exercicios/Exercicios.java
@@ -7,6 +7,23 @@ package com.reis.exercicios;
 public class Exercicios {
 
     public static void main(String[] args) {
+
+        Pessoa[] pessoas = new Pessoa[3];
+        Livro[] livros = new Livro[3];
+
+        pessoas[0] = new Pessoa("Reis", 33, 'M');
+        pessoas[1] = new Pessoa("Zarpelon", 31, 'F');
+        pessoas[2] = new Pessoa("De Marchi", 12, 'F');
+
+        livros[0] = new Livro("Programas Não Esquecidos", "Java", 128, pessoas[0]);
+        livros[1] = new Livro("A Arte Moderna", "CSS", 256, pessoas[1]);
+        livros[2] = new Livro("Marcações Profundas", "HTML", 64, pessoas[2]);
+
+        livros[0].avançarPagina();
+        livros[0].abrir();
+        livros[0].avançarPagina();        
+        livros[0].avançarPagina();
         
+
     }
 }
