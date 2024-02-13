10077d4a1f331f86504a62bc6d509d0d5d2235b7
Reis <aglemar.reis@gmail.com>
2024-02-13 15:17:49 -0300

    up

---
+++ Heranca/src/main/java/com/reis/main/Heranca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.reis.main;
+
+/**
+ *
+ * @author reis
+ */
+public class Heranca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
