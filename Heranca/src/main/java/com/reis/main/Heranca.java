5ac9cf09b490a4ae57e52acc49915f0446e1d48e
Reis <aglemar.reis@gmail.com>
2024-02-13 16:40:52 -0300

    novas chamadas na main

--- Heranca/src/main/java/com/reis/main/Heranca.java
+++ Heranca/src/main/java/com/reis/main/Heranca.java
@@ -11,6 +11,40 @@ package com.reis.main;
 public class Heranca {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Pessoa pessoa = new Pessoa();
+        Aluno aluno1 = new Aluno();
+        Professor professor1 = new Professor();
+        Funcionario funcionario1 = new Funcionario();
+        
+        aluno1.setNome("João Da Silva");
+        aluno1.setIdade(23);
+        aluno1.setSexo("M");
+        aluno1.setCurso("Administração");
+        aluno1.setMatricula(123);
+        
+        professor1.setNome("Pedro De Paula");
+        professor1.setIdade(50);
+        professor1.setSexo("M");
+        professor1.setEspecialidade("Programador");
+        professor1.setSalario(6500.00f);
+        
+        funcionario1.setNome("Maria Da Luz");
+        funcionario1.setIdade(32);
+        funcionario1.setSexo("F");
+        funcionario1.setSetor("Administração");
+        funcionario1.setTrabalhando(true);
+        
+        System.out.println(funcionario1.getNome());
+        System.out.println(funcionario1.isTrabalhando());
+        System.out.println(funcionario1.getSetor());
+        funcionario1.mudarSetor("Secertaria");
+        System.out.println(funcionario1.getSetor());
+        funcionario1.fazerAniversario();
+        System.out.println(funcionario1.toString());
+        System.out.println(funcionario1.toStringFuncionario());
+        
+        System.out.println(professor1.toString());
+        System.out.println(professor1.toStringProfessor());
     }
 }
