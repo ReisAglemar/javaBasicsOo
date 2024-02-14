6dae2348f93a830a9a7dd54d9cf1ae97ae3a964f
Reis <aglemar.reis@gmail.com>
2024-02-14 11:15:07 -0300

    Criado a classe visitante, herança de implementacão

---
+++ HerancaPt2/src/main/java/com/reis/herancapt2/Visitante.java
@@ -0,0 +1,11 @@
+package com.reis.herancapt2;
+
+/**
+ *
+ * @author reis
+ */
+public class Visitante extends Pessoa {
+}
+
+//EXEMPLO DE HERANÇA DE IMPLEMENTAÇÃO.
+//NESSA HERANÇA SÓ HÁ A HERANÇA EM SI, SEM NADA MAIS.
