2d6a01d9d6d09809f1bf3b1025c9410576fb42b9
Reis <aglemar.reis@gmail.com>
2024-02-14 10:12:13 -0300

    Novo projeto

---
+++ HerancaPt2/src/main/java/com/reis/herancapt2/HerancaPt2.java
@@ -0,0 +1,12 @@
+package com.reis.herancapt2;
+
+/**
+ *
+ * @author reis
+ */
+public class HerancaPt2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
