5991ec24a443ea76dd29ddf3719ac984ba569ed5
Reis <aglemar.reis@gmail.com>
2024-02-15 11:50:51 -0300

    ajustado indentação

--- HerancaPt2/src/main/java/com/reis/herancapt2/Visitante.java
+++ HerancaPt2/src/main/java/com/reis/herancapt2/Visitante.java
@@ -4,7 +4,7 @@ package com.reis.herancapt2;
  *
  * @author reis
  */
-public class Visitante extends Pessoa {
+public final class Visitante extends Pessoa {
 }
 
 //EXEMPLO DE HERANÇA DE IMPLEMENTAÇÃO.
