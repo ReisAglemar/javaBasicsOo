3a5348b9cce98191c9428161f068e379c23ab51e
Reis <aglemar.reis@gmail.com>
2024-02-16 09:42:14 -0300

    começando

---
+++ Polimorfismo/src/main/java/com/reis/polimorfismo/Polimorfismo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.reis.polimorfismo;
+
+/**
+ *
+ * @author reis
+ */
+public class Polimorfismo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
